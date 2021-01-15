981dc83b3863c860a5e5e74215e116b3a5afe817
N-bred <thehardyboiz1@gmail.com>
2021-01-15 02:11:28 -0500

    Finish solution for moving zeros problem

--- kyu5/MovingZeros.js
+++ kyu5/MovingZeros.js
@@ -1,5 +1,23 @@
 // https://www.codewars.com/kata/52597aa56021e91c93000cb0/javascript
 
-function moveZeros(arr) {}
+// function moveZeros(arr) {
+//   if (!arr || arr.length < 1) return null
+
+//   const zerosArr = []
+
+//   for (let i = arr.length - 1; i >= 0; --i) {
+//     if (arr[i] === 0) {
+//       zerosArr.push(arr.splice(i, 1)[0])
+//     }
+//   }
+
+//   return arr.concat(zerosArr)
+// }
+
+function moveZeros(arr) {
+  if (!arr || arr.length < 1) return null
+  let length = 0
+  return arr.filter((e) => (e === 0 ? length++ && false : true)).concat(Array.from({ length }, () => 0))
+}
 
 module.exports = moveZeros
--- tests/MovingZeros.test.js
+++ tests/MovingZeros.test.js
@@ -2,5 +2,23 @@ const { moveZeros } = require('../kyu5')
 const { expect } = require('@jest/globals')
 
 test('Takes an array with multiple data type values, should return an array with all the values equal to 0, to the end while preserving the order of the original array', () => {
-  expect(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a'])).toEqual([false, 1, 1, 2, 1, 3, 'a'])
+  expect(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a'])).toEqual(expect.arrayContaining([false, 1, 1, 2, 1, 3, 'a', 0, 0]))
+})
+
+test('2 Takes an array with multiple data type values, should return an array with all the values equal to 0, to the end while preserving the order of the original array', () => {
+  expect(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, 'a', 'b', 0, 'c', 1, 4, 5, 6, 7, 0, 'x', 2, { a: 'value' }])).toEqual(
+    expect.arrayContaining([false, 1, 1, 2, 1, 3, 'a', 'b', 'c', 1, 4, 5, 6, 7, 'x', 2, { a: 'value' }, 0, 0, 0, 0])
+  )
+})
+
+test('3 Takes an array with multiple data type values, should return an array with all the values equal to 0, to the end while preserving the order of the original array', () => {
+  expect(moveZeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9])).toEqual(
+    expect.arrayContaining([9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  )
+})
+
+test('3 Takes an array with multiple data type values, should return an array with all the values equal to 0, to the end while preserving the order of the original array', () => {
+  expect(moveZeros(['a', 0, 0, 'b', null, 'c', 'd', 0, 1, false, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9])).toEqual(
+    expect.arrayContaining(['a', 'b', null, 'c', 'd', 1, false, 1, 3, [], 1, 9, {}, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  )
 })
