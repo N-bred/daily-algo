14dfdaf38a2dd806b533058ac2dbfca9b42dbc4a
N-bred <thehardyboiz1@gmail.com>
2021-01-16 01:50:44 -0500

    Solved variance problem with both O(n*2) and O(n) solutions

--- kyu5/CalculateVariance.js
+++ kyu5/CalculateVariance.js
@@ -1,5 +1,25 @@
 // https://www.codewars.com/kata/5266fba01283974e720000fa
 
-function variance(nArr) {}
+// O(n*2)
+// function variance(nArr) {
+//   const mean = nArr.reduce((a, b) => a + b, 0) / nArr.length
+//   return nArr.reduce((a, b) => a + (b - mean) ** 2, 0) / nArr.length
+// }
+
+// O(n)
+function variance(nArr) {
+  const length = nArr.length
+  let sum = 0
+  let mult = 0
+
+  for (let i = 0; i < length; ++i) {
+    sum += nArr[i]
+    mult += nArr[i] ** 2
+  }
+
+  return mult / length - (sum / length) ** 2
+}
 
 module.exports = variance
+
+// sum of (x ** 2 / n) - mean **2
--- tests/CalculateVariance.test.js
+++ tests/CalculateVariance.test.js
@@ -3,5 +3,5 @@ const { expect } = require('@jest/globals')
 
 test('Receives an array of numbers and returns its variance', () => {
   const INPUT = [-10, 0, 10, 20, 30]
-  expect(variance(INPUT)).toEqual('200.0000')
+  expect(variance(INPUT).toFixed(4)).toEqual('200.0000')
 })
