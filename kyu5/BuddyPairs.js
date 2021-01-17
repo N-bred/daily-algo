c7a6c5567485b1dc335b2174db2c02df07b27039
N-bred <thehardyboiz1@gmail.com>
2021-01-17 00:32:46 -0500

    Finished solution for the Buddy pairs problem

--- kyu5/BuddyPairs.js
+++ kyu5/BuddyPairs.js
@@ -1,5 +1,25 @@
 // https://www.codewars.com/kata/59ccf051dcc4050f7800008f
 
-function buddy(min, max) {}
+function getSumOfProperDivisors(n) {
+  let divisors = 0
+  for (let i = 0; i < n; ++i) {
+    if (n % i === 0) {
+      divisors += i
+    }
+  }
+  return divisors
+}
+
+function buddy(min, max) {
+  for (let i = min; i <= max; ++i) {
+    const sum = getSumOfProperDivisors(i)
+    if (sum > i) {
+      const otherSum = getSumOfProperDivisors(sum - 1)
+      if (otherSum - 1 === i) {
+        return [i, sum - 1]
+      }
+    }
+  }
+}
 
 module.exports = buddy
