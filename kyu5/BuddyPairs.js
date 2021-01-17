48472082db677ea8a6d9f3e0e2b4c6b8f6979b9d
N-bred <thehardyboiz1@gmail.com>
2021-01-17 01:20:11 -0500

    Added test for the buddy pairs problem and refactored code

--- kyu5/BuddyPairs.js
+++ kyu5/BuddyPairs.js
@@ -1,10 +1,15 @@
 // https://www.codewars.com/kata/59ccf051dcc4050f7800008f
 
 function getSumOfProperDivisors(n) {
+  if (n === 1) return 1
   let divisors = 0
-  for (let i = 0; i < n; ++i) {
+  for (let i = 2; i <= Math.sqrt(n); ++i) {
     if (n % i === 0) {
-      divisors += i
+      if (i === n / i) {
+        divisors += i
+      } else {
+        divisors += i + n / i
+      }
     }
   }
   return divisors
@@ -14,12 +19,13 @@ function buddy(min, max) {
   for (let i = min; i <= max; ++i) {
     const sum = getSumOfProperDivisors(i)
     if (sum > i) {
-      const otherSum = getSumOfProperDivisors(sum - 1)
-      if (otherSum - 1 === i) {
-        return [i, sum - 1]
+      const otherSum = getSumOfProperDivisors(sum)
+      if (otherSum === i) {
+        return [i, sum]
       }
     }
   }
+  return 'Nothing'
 }
 
 module.exports = buddy
--- tests/BuddyPairs.test.js
+++ tests/BuddyPairs.test.js
@@ -4,3 +4,7 @@ const { expect } = require('@jest/globals')
 test('Receives two numbers denoting the limits to look for the numbers to return, should return a pair of numbers which are buddies', () => {
   expect(buddy(23, 4669)).toEqual(expect.arrayContaining([48, 75]))
 })
+
+test('Receives two numbers denoting the limits to look for the numbers to return, should return a pair of numbers which are buddies', () => {
+  expect(buddy(4700, 7000)).toEqual(expect.arrayContaining([5775, 6128]))
+})
