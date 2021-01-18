04ded0911bb596d7fa3ba6231b53cb6fea19ae15
N-bred <thehardyboiz1@gmail.com>
2021-01-18 01:15:33 -0500

    Finish solution for FactorialDecomposition problem

--- kyu5/FactorialDecomposition.js
+++ kyu5/FactorialDecomposition.js
@@ -2,10 +2,55 @@
 
 // Make Prime numbers list
 
+function isPrime(n) {
+  for (let i = 2; i < n; ++i) {
+    if (n % i === 0) return false
+  }
+  return true
+}
+
+function getPrimeNumbers(n) {
+  const list = [2]
+  for (let i = 3; i < n; i += 2) {
+    if (isPrime(i)) {
+      list.push(i)
+    }
+  }
+  return list
+}
+
 // Make factorial function
+
+function factorial(n) {
+  if (typeof n === 'number') n = BigInt(n)
+  if (n <= 1n) return 1n
+  return n * factorial(n - 1n)
+}
 
 // Do logic
 
-function decomp(n) {}
+function decomp(n) {
+  const primeNumbers = getPrimeNumbers(n)
+  const factorialNumber = factorial(n)
+  const primeFactors = []
+  let exp = 1
+
+  for (number of primeNumbers) {
+    const bigNumber = BigInt(number)
+    if (factorialNumber % bigNumber !== 0n) break
+
+    let result = factorialNumber / bigNumber
+
+    while (result % bigNumber === 0n) {
+      exp++
+      result /= bigNumber
+    }
+
+    primeFactors.push(`${number}${exp > 1 ? `^${exp}` : ''}`)
+    exp = 1
+  }
+
+  return primeFactors.join(' * ')
+}
 
 module.exports = decomp
