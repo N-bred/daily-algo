aa3fd8e7d3f3c2172df61d6a7e81686e68644597
N-bred <thehardyboiz1@gmail.com>
2021-01-18 01:16:07 -0500

    Added test for factorialDecomposition problem

--- tests/FactorialDecomposition.test.js
+++ tests/FactorialDecomposition.test.js
@@ -4,3 +4,7 @@ const { expect } = require('@jest/globals')
 test("Decomp takes a number should return the number's factorial decomposed into its prime factors", () => {
   expect(decomp(12)).toEqual('2^10 * 3^5 * 5^2 * 7 * 11')
 })
+
+test("Decomp takes a number should return the number's factorial decomposed into its prime factors", () => {
+  expect(decomp(25)).toEqual('2^22 * 3^10 * 5^6 * 7^3 * 11^2 * 13 * 17 * 19 * 23')
+})
