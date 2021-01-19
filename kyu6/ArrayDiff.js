8d94b202481bbd2f3ae527c658e90646a5f0a6cc
N-bred <thehardyboiz1@gmail.com>
2021-01-19 03:05:47 -0500

    Finish solution for ArrayDiff problem

--- kyu6/ArrayDiff.js
+++ kyu6/ArrayDiff.js
@@ -1,5 +1,14 @@
 // https://www.codewars.com/kata/523f5d21c841566fde000009
 
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+  const freq = {}
+  for (let i = 0; i < b.length; ++i) freq[b[i]] = 1
+
+  for (let i = a.length - 1; i >= 0; --i) {
+    if (freq[a[i]]) a.splice(i, 1)
+  }
+
+  return a
+}
 
 module.exports = arrayDiff
