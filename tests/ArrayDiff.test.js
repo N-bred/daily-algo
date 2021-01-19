4c4da2744b58b34e75710fc773e09079c1976373
N-bred <thehardyboiz1@gmail.com>
2021-01-19 03:06:49 -0500

    Added test for arrayDiff problem

--- tests/ArrayDiff.test.js
+++ tests/ArrayDiff.test.js
@@ -5,3 +5,7 @@ const { expect } = require('@jest/globals')
 test('Receives two arrays and returns one with the difference between the two inputs', () => {
   expect(arrayDiff([3, 4], [3])).toEqual(expect.arrayContaining([4]))
 })
+
+test('Receives two arrays and returns one with the difference between the two inputs', () => {
+  expect(arrayDiff([1, 8, 2], [])).toEqual(expect.arrayContaining([]))
+})
