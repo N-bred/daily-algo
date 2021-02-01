ac636059dd71582df27df46b1d5190279331cf4b
N-bred <thehardyboiz1@gmail.com>
2021-02-01 17:53:51 -0500

    Added new Test for BreakCamelCase

--- tests/BreakCamelCase.test.js
+++ tests/BreakCamelCase.test.js
@@ -3,3 +3,7 @@ const { BreakCamelCase } = require('../kyu6')
 test('Receives a string in camel case and should return the string splittled', () => {
   expect(BreakCamelCase('camelCase')).toEqual('camel Case')
 })
+
+test('Receives a string in camel case and should return the string splittled', () => {
+  expect(BreakCamelCase('camelCasing')).toEqual('camel Casing')
+})
