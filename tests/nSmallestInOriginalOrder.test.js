48a95d5af9dc8c55361c4830beb397ed9a884d8a
N-bred <thehardyboiz1@gmail.com>
2021-02-03 18:13:20 -0500

    Added test for SmallestInOriginalFunction

--- tests/nSmallestInOriginalOrder.test.js
+++ tests/nSmallestInOriginalOrder.test.js
@@ -3,3 +3,7 @@ const { firstNSmallest } = require('../kyu6')
 test('Receives an array and returns the smallest numbers in it, in the original order', () => {
   expect(firstNSmallest([5, 4, 3, 2, 1], 3)).toEqual([3, 2, 1])
 })
+
+test('Receives an array and returns the smallest numbers in it, in the original order', () => {
+  expect(firstNSmallest([1, 2, 3, -4, 0], 3)).toEqual([1, -4, 0])
+})
