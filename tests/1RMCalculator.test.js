d7c11682327603cc051beb768af76abe934f5479
N-bred <thehardyboiz1@gmail.com>
2021-02-02 20:59:08 -0500

    added test for 1rmcalculator

--- tests/1RMCalculator.test.js
+++ tests/1RMCalculator.test.js
@@ -3,3 +3,7 @@ const { calculate1RM } = require('../kyu6')
 test('Receives an array with w and r and should return the correct value', () => {
   expect(calculate1RM([135, 20])).toEqual(282)
 })
+
+test('Receives an array with w and r and should return the correct value', () => {
+  expect(calculate1RM([200, 8])).toEqual(253)
+})
