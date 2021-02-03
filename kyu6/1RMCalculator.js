f7c97988a32aa5f5f7750d1381fcdff83e5103b3
N-bred <thehardyboiz1@gmail.com>
2021-02-02 21:52:08 -0500

    Added two more tests that were causing the solution to fail, and fixed the bug, finished solution

--- kyu6/1RMCalculator.js
+++ kyu6/1RMCalculator.js
@@ -1,7 +1,12 @@
 // https://www.codewars.com/kata/595bbea8a930ac0b91000130
 
 function calculate1RM(w, r) {
-  return // the 1RM as specified in the description, or w if r == 1, or null if r == 0
+  if (r === 0) return 0
+  if (r === 1) return w
+  const a = (w, r) => w * (1 + r / 30)
+  const b = (w, r) => (w * 100) / (101.3 - 2.67123 * r)
+  const c = (w, r) => w * r ** 0.1
+  return Math.round([a, b, c].map((f) => f(w, r)).sort((a, b) => b - a)[0])
 }
 
 module.exports = calculate1RM
--- tests/1RMCalculator.test.js
+++ tests/1RMCalculator.test.js
@@ -1,12 +1,20 @@
 const { calculate1RM } = require('../kyu6')
 
 test('Receives an array with w and r and should return the correct value', () => {
-  expect(calculate1RM([135, 20])).toEqual(282)
+  expect(calculate1RM(135, 20)).toEqual(282)
 })
 
 test('Receives an array with w and r and should return the correct value', () => {
-  expect(calculate1RM([200, 8])).toEqual(253)
+  expect(calculate1RM(200, 8)).toEqual(253)
 })
 test('Receives an array with w and r and should return the correct value', () => {
-  expect(calculate1RM([400, 0])).toEqual(0)
+  expect(calculate1RM(400, 0)).toEqual(0)
+})
+
+test('Receives an array with w and r and should return the correct value', () => {
+  expect(calculate1RM(270, 2)).toEqual(289)
+})
+
+test('Receives an array with w and r and should return the correct value', () => {
+  expect(calculate1RM(234, 3)).toEqual(261)
 })
