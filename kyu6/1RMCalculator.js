7b8659527df5cbfc99452e5aa59ec5814690c241
N-bred <thehardyboiz1@gmail.com>
2021-02-02 20:58:30 -0500

    Added test for 1rmcalcultator

--- kyu6/1RMCalculator.js
+++ kyu6/1RMCalculator.js
@@ -1,1 +1,7 @@
 // https://www.codewars.com/kata/595bbea8a930ac0b91000130
+
+function calculate1RM(w, r) {
+  return // the 1RM as specified in the description, or w if r == 1, or null if r == 0
+}
+
+module.exports = calculate1RM
--- kyu6/index.js
+++ kyu6/index.js
@@ -1,7 +1,9 @@
 const arrayDiff = require('./ArrayDiff')
 const BreakCamelCase = require('./BreakCamelCase')
+const calculate1RM = require('./1RMCalculator')
 
 module.exports = {
   arrayDiff,
   BreakCamelCase,
+  calculate1RM,
 }
