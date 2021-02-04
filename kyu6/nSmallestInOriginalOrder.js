41f0be9e912564ce45420fe06e68ffdd77daa6ac
N-bred <thehardyboiz1@gmail.com>
2021-02-03 21:36:28 -0500

    Solution for nSmallestOriginalOrder challenge

--- kyu6/nSmallestInOriginalOrder.js
+++ kyu6/nSmallestInOriginalOrder.js
@@ -1,7 +1,10 @@
 // https://www.codewars.com/kata/5aec1ed7de4c7f3517000079
 
 function firstNSmallest(array, n) {
-  //your code here
+  while (array.length !== n) {
+    array.splice(array.lastIndexOf(Math.max(...array)), 1)
+  }
+  return array
 }
 
 module.exports = firstNSmallest
